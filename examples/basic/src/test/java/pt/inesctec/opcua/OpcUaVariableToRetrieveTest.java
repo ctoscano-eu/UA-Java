0b2f711762339c19de3adc09dff9921ad73888fa
ctoscano <ctoscano@inesctec.pt>
2017-12-27 15:56:29 +0000

    .

--- examples/basic/src/test/java/pt/inesctec/opcua/OpcUaVariableToRetrieveTest.java
+++ examples/basic/src/test/java/pt/inesctec/opcua/OpcUaVariableToRetrieveTest.java
@@ -1,8 +1,12 @@
 package pt.inesctec.opcua;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.opcfoundation.ua.builtintypes.DataValue;
+import org.opcfoundation.ua.builtintypes.StatusCode;
 import org.opcfoundation.ua.common.ServiceFaultException;
 import org.opcfoundation.ua.common.ServiceResultException;
 
@@ -24,8 +28,14 @@ public class OpcUaVariableToRetrieveTest {
 	}
 
 	@Test
-	public void testBrowseNodeOjectsAndVariables() {
+	public void testReadVariablesFromSameServer() {
 		try {
+			String[] variables = new String[] { "/Objects/Server/ServerArray", "/Objects/Server/NamespaceArray", "/Objects/Server/ServerStatus", "/Objects/Server/ServerStatus" };
+
+			DataValue[] res = opcUaClient.readVariableValue("opc.tcp://localhost:4334/UA/teste", variables);
+			assertEquals(4, res.length);
+			for (int i = 0; i < res.length; ++i)
+				assertEquals(StatusCode.GOOD, res[i].getStatusCode());
 		}
 		catch (Throwable t) {
 			t.printStackTrace();
